83465cc4de1556e36de264469e0875823abc3d79
Arthur Machado <machadoparthur1@gmail.com>
2025-02-05 16:15:04 -0300

    constant time O(1)

--- bigO/findNemoBigO.js
+++ bigO/findNemoBigO.js
@@ -1,16 +1,20 @@
 const nemo = ['nemo'];
 const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank']
-const largeArray = new Array(100000).fill('nemo')
+const largeArray = new Array(10000).fill('nemo')
 
 function findNemo(array) {
-  const t0 = performance.now();
   for (let i = 0; i < array.length; i++) {
     if (array[i] === 'nemo') {
       console.log('Found nemo');
     }
   }
-  const t1 = performance.now();
-  console.log('Call to find nemo took' + (t1 - t0) + 'milliseconds')
 }
 
-findNemo(largeArray);
+// findNemo(largeArray); // O(n) --> Linear Time
+
+function logFirstTwoFishes(fishes) {
+  console.log(fishes[0]); // O(1)
+  console.log(fishes[1]); // O(1)
+}
+
+logFirstTwoFishes(everyone) // O(2)
