584e79f62770a77762877533118ea8065caa1912
Arthur Machado <machadoparthur1@gmail.com>
2025-04-17 12:20:48 -0300

    explore BFS

---
+++ algorithms/searching/interview.js
@@ -0,0 +1,11 @@
+// If you know a solution is not far from the root of the tree: BFS
+
+// If the tree is very deep and solutions are rare: BFS, because with DFS it will be time consuming
+
+// If the tree is very wide: DFS, because BFS will need too much memory to store the entire tree
+
+// If solutions are frequent but located deep in the tree: DFS
+
+// Determining whether a path exists between two nodes: DFS
+
+// Finding the shortest path: BFS
